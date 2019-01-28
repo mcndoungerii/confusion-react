802f1fc71f33edc4237b9c895c055f1ad1996082
Allen Mwandunga <allenmcndounger@gmail.com>
2019-01-28 13:05:22 +0300

    Task 2 on Assignment 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -11,6 +11,25 @@ class Dishdetail extends Component {
         }
     }
 
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render() {
         return (
             <div className="container">
