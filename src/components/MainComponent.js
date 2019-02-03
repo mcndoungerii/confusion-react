5d14f620ebae80a7c3047fed8af74aaac0fb4a67
Allen Mwandunga <allenmcndounger@gmail.com>
2019-02-03 09:52:56 +0300

    Single Page Application Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -28,18 +28,29 @@ class Main extends Component {
   render() {
 
     const HomePage = () => {
-        return (
-            <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                  promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                  leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
-        );
-    }  
+      return (
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]} 
+        />
+      );
+    }
+
+    const DishWithId = ({match}) => {
+      return(
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        />
+      )
+    }
+
     return (
       <div> 
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route path='/contactus' component={Contact} />
           <Redirect to="/home" />
         </Switch>
