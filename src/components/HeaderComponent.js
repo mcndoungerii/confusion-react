233824d7e53ce8c7a634ba9bd67e4ca88f5a5edc
Allen Mwandunga <allenmcndounger@gmail.com>
2019-02-05 00:26:20 +0300

    Uncontrolled Form --> Importing the Button, Modal, ModalHeader, ModalBody and setting up the toggleModal(){} method

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -7,14 +8,23 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
         this.togglerNav = this.togglerNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+
     }
 
     togglerNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
         });
     }
 
@@ -51,6 +61,12 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                            </Nav>
+                            
                         </Collapse>
                     </div> 
                 </Navbar>
@@ -64,6 +80,12 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron> 
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
