ae885cfea586a85be9f9924ccb2bdf96b66927d0
Allen Mwandunga <allenmcndounger@gmail.com>
2019-02-05 00:43:47 +0300

    Uncontrolled Form --> Adding the Uncontrolled Form in the <ModalBody> </ModalBody> tag, setting up the handleLogin(){} function, invoking it to the <Modal> Tag and binding it and retrieving info using a innerRef{}

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -13,6 +13,7 @@ class Header extends Component {
         };
         this.togglerNav = this.togglerNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
 
     }
 
@@ -26,6 +27,14 @@ class Header extends Component {
         this.setState({
           isModalOpen: !this.state.isModalOpen
         });
+    }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
     }
 
     render() {
@@ -83,7 +92,26 @@ class Header extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
-                    
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </React.Fragment>
