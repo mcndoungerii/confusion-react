ce4ca3b22cf3fb7d91f9c42a7aa67315bc1b0a7f
Allen Mwandunga <allenmcndounger@gmail.com>
2019-02-14 13:19:46 +0300

    Task on Updating Redux actions, Home and About Component to fetch data from the server to render Leaders, also Task on Saving Feedback data submited by user at the Form in the Contact Component and Also Task on introducing react-animation-components (stagger, fade) to the About Component to render Leaders with animations

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,32 +1,61 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
-function RenderLeader({ leader }) {
+function RenderLeader({ leaders, isLoading, errMess }) {
+
+    if (isLoading) {
+            return(           
+                <Loading />     
+            );
+        }
+    else if (errMess) {
+        return(
+            <h4>{errMess}</h4>
+        );
+    }
+    else {
+        return(
+            <Stagger in>{
+                leaders = leaders.map((leader) => {   
+                    return (
+                        <Fade in>
+                            <div key={leader.id} className="col-12 mt-5">
+                                <Media tag="li">
+                                    <Media left middle>
+                                        <Media object src={baseUrl + leader.image} alt={leader.name} />
+                                    </Media>
+                                    <Fade in>
+                                        <Media body className="ml-5">
+                                            <Media heading>{leader.name}</Media>
+                                            <p>{leader.designation}</p>
+                                            <p>{leader.description}</p>
+                                        </Media>
+                                    </Fade>
+                                </Media>
+                            </div>
+                        </Fade>
+                    );   
+                })}
+            </Stagger>
+        );    
+    }
     return (
-        <div key={leader.id} className="col-12 mt-5">
-            <Media tag="li">
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
+        <div>
+            <Media list>
+                {leaders}
             </Media>
-        </div>
-    );   
+        </div> 
+    )
+    console.log();
 } 
 
 function About(props) {
-
-    const leaders = props.leaders.map((leader) => {
-        return (
-            <RenderLeader leader={leader} />
-        );
-    });
-
+    
+    
     return(
         <div className="container">
             <div className="row">
@@ -83,7 +112,8 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        <RenderLeader leaders={props.leaders.leaders} 
+                        isLoading={props.leaders.isLoading} errMess={props.leaders.errMess} />
                     </Media>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import { postComment, postFeedback, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -24,10 +24,13 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+  postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) =>
+    dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message)),
   fetchDishes: () => { dispatch(fetchDishes())},
   resetFeedbackForm: () => { dispatch(actions.reset('feedback'))},
   fetchComments: () => dispatch(fetchComments()),
-  fetchPromos: () => dispatch(fetchPromos())
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders())
 });
 
 class Main extends Component {
@@ -41,6 +44,7 @@ class Main extends Component {
     this.props.fetchDishes();
     this.props.fetchComments();
     this.props.fetchPromos();
+    this.props.fetchLeaders();
   }
 
   render() {
@@ -53,7 +57,9 @@ class Main extends Component {
           promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
           promoLoading={this.props.promotions.isLoading}
           promoErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.filter((leader) => leader.featured)[0]} 
+          leader={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} 
+          leaderLoading={this.props.leaders.isLoading}
+          leaderErrMess={this.props.leaders.errMess}
         />
       );
     }
@@ -67,6 +73,7 @@ class Main extends Component {
           postComment={this.props.postComment}
           commentsErrMess={this.props.comments.errMess}
           postComment={this.props.postComment}
+          
         />
       )
     }
@@ -81,7 +88,7 @@ class Main extends Component {
               <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+              <Route path='/contactus' component={() => <Contact postFeedback={this.props.postFeedback} resetFeedbackForm={this.props.resetFeedbackForm} />} />
               <Redirect to="/home" />
             </Switch>
           </CSSTransition>
