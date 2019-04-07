7ce73a10eb34b4af8a7aa3dc59832490504c4d27
myUserName <myName@email.com>
2019-04-07 17:53:45 +0300

    Login and Register Forms (New)

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label } from 'reactstrap';
+    Label, Col, Row } from 'reactstrap';
+
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { NavLink } from 'react-router-dom';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+const validPassword = (val) => /^[A-Z0-9._%+-]+[A-Z0-9.-]+\[A-Z]{2,4}$/i.test(val);
 
 class Header extends Component {
 
@@ -9,12 +18,14 @@ class Header extends Component {
         super(props);
         this.state = {
             isNavOpen: false,
-            isModalOpen: false
+            isModalLoginOpen: false,
+            isModalRegisterOpen: false,
         };
         this.togglerNav = this.togglerNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
+        this.toggleModalLogin = this.toggleModalLogin.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
-
+        this.toggleModalRegister = this.toggleModalRegister.bind(this);
+        this.handleRegister = this.handleRegister.bind(this);
     }
 
     togglerNav() {
@@ -23,17 +34,31 @@ class Header extends Component {
         });
     }
 
-    toggleModal() {
+    toggleModalLogin() {
         this.setState({
-          isModalOpen: !this.state.isModalOpen
+          isModalLoginOpen: !this.state.isModalLoginOpen
+        });
+    }
+    toggleModalRegister() {
+        this.setState({
+         isModalRegisterOpen: !this.state.isModalRegisterOpen
         });
     }
 
-    handleLogin(event) {
-        this.toggleModal();
-        alert("Username: " + this.username.value + " Password: " + this.password.value
-            + " Remember: " + this.remember.checked);
-        event.preventDefault();
+    handleLogin(values) {
+        this.toggleModalLogin();
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetLoginForm();
+        this.props.postLogin(values.username, values.email, values.agree, values.password);
+
+    }
+    handleRegister(values) {
+        this.toggleModalRegister();
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetRegisterForm();
+        this.props.postRegister(values.username, values.firstname, values.lastname, values.email, values.password);
 
     }
 
@@ -72,10 +97,12 @@ class Header extends Component {
                             </Nav>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                    <Button outline onClick={this.toggleModalLogin}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                                </NavItem>
+                                 <NavItem>
+                                    <Button outline onClick={this.toggleModalRegister}><span className="fa fa-user-plus fa-lg"></span> Register</Button>
                                 </NavItem>
                             </Nav>
-                            
                         </Collapse>
                     </div> 
                 </Navbar>
@@ -89,28 +116,191 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron> 
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                <Modal isOpen={this.state.isModalLoginOpen} toggle={this.toggleModalLogin}>
+                    <ModalHeader toggle={this.toggleModalLogin}>Login</ModalHeader>
                     <ModalBody>
-                        <Form onSubmit={this.handleLogin}>
-                            <FormGroup>
-                                <Label htmlFor="username">Username</Label>
-                                <Input type="text" id="username" name="username"
-                                    innerRef={(input) => this.username = input} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="password">Password</Label>
-                                <Input type="password" id="password" name="password"
-                                    innerRef={(input) => this.password = input}  />
-                            </FormGroup>
-                            <FormGroup check>
-                                <Label check>
-                                    <Input type="checkbox" name="remember"
-                                    innerRef={(input) => this.remember = input}  />
-                                    Remember me
-                                </Label>
-                            </FormGroup>
-                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        <Form model="register" onSubmit={(values) => this.Login(values)}>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".username" id="username" name="username"
+                                        placeholder="User Name or Email Address"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".username"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>                        
+                            </Row>
+                            
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".password" id="password" name="password"
+                                        placeholder="Password"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".password"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <div className="form-check">
+                                        <Label check>
+                                            <Control.checkbox model=".agree"
+                                                name="agree"
+                                                className="form-check-input" /> {' '}
+                                            <strong>Remember me</strong>
+                                        </Label>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Button type="submit" color="primary">
+                                        Login
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+
+                <Modal isOpen={this.state.isModalRegisterOpen} toggle={this.toggleModalRegister}>
+                    <ModalHeader toggle={this.toggleModalRegister}>Register</ModalHeader>
+                    <ModalBody>
+                        <Form model="register" onSubmit={(values) => this.handleRegister(values)}>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".username" id="username" name="username"
+                                        placeholder="User Name"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".username"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>                        
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".firstname" id="firstname" name="firstname"
+                                        placeholder="First Name"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />            
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".lastname" id="lastname" name="lastname"
+                                        placeholder="Last Name"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>                        
+                            </Row>
+                            <Row className="form-group">
+                                    <Col>
+                                        <Control.text model=".email" id="email" name="email"
+                                            placeholder="Email"
+                                            className="form-control" 
+                                            validators={{
+                                            required, validEmail
+                                            }}
+                                        />
+                                        <Errors
+                                            className="text-danger"
+                                            model=".email"
+                                            show="touched"
+                                            messages={{
+                                                required: ' Required ',                                                
+                                                validEmail: 'Invalid Email Address'
+                                            }}
+                                        />                                           
+                                    </Col>
+                            </Row>
+                            <Row className="form-group">
+                                    <Col>
+                                        <Control.text model=".password" id="password" name="password"
+                                        placeholder="Password"
+                                        className="form-control" 
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }} 
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".password"
+                                        show="touched"
+                                        messages={{
+                                            required: ' Required ',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />                                           
+                                    </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Button type="submit" value="submit" color="primary">
+                                        Register
+                                    </Button>
+                                </Col>
+                            </Row>
                         </Form>
                     </ModalBody>
                 </Modal>
